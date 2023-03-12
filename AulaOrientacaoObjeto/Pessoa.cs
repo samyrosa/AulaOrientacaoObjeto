fc047f649fcea671f47eca8eb8df3af3409ea8c8
samy-rosa <sr.oliveira0910@gmail.com>
2023-03-12 10:16:31 -0300

    Criação da Classe Pessoa, seus atributos e do metodo contrutor

---
+++ AulaOrientacaoObjeto/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaOrientacaoObjeto
+{
+    public class Pessoa
+    {
+        private int Id;
+        private string Name;
+        private string Telefone;
+
+        public Pessoa(int id, string name, string telefone)
+        {
+            Id = id;
+            Name = name;
+            Telefone = telefone;
+        }
+    }
+}
